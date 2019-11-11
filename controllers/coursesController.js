b60cc3f93856718a5b84e526777ffda776395ae5
Daggron <returnofking04@gmail.com>
2019-11-12 00:43:06 +0530

    Added models

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -1,12 +1,3 @@
 exports.getAll = (req,res)=>{
-    const courses = [
-        {
-            "title":"Android",
-            "Description":"A course for the beiginners for android",
-            "Author":"Mitch",
-            "Github Url":"daggron"
-        }
-    ]
-
-    res.status(200).json(courses);
+    
 }
---
+++ models/courses.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+let schema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    Instructor:{
+        type:String,
+        required:true
+    },
+    Description:{
+        type:String,
+        required:true
+    },
+    Url:{
+        type:String,
+    },
+    Language:{
+        type:String,
+    },
+    Date:{
+        type:String,
+        required:true,
+        default:Date.now()
+    },
+    Github:{
+        type:String
+    },
+    image:{
+        type:String
+    },
+    Level:{
+        type:String
+    }
+
+},{
+    timestamps:true
+});
+
+
+module.exports = mongoose.model('Courses',schema);
