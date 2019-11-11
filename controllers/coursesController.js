aea8fecc9559dd32eab6593981ed1a350bbde27a
Daggron <returnofking04@gmail.com>
2019-11-12 02:20:36 +0530

    Added get by id and delete routes

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -1,5 +1,40 @@
 let Courses = require('../models/courses.model');
 
+exports.getCourses = (req,res)=>{
+    Courses.find()
+    .exec()
+    .then(courses=>{
+        res.status(200).json(courses);
+    })
+    .catch(err=>{
+        console.log('Code Red'+err);
+        res.status(404).json({
+            message:err
+        });
+    });
+}
+
+exports.getById = (req,res)=>{
+    Courses.findById(req.params.id)
+    .exec()
+    .then(course=>{
+        if(course){
+            res.status(200).json(course)
+        }
+        else{
+            res.status(404).json({
+                message:'Course Not Found'
+            })
+        }
+    })
+    .catch(err=>{
+        console.error(err)
+        res.status(500).json({
+            message:'Internal Server Error'
+        })
+    })
+}
+
 exports.postCourse = (req,res)=>{
     let course = new Courses();
     course.title = req.body.title;
@@ -21,6 +56,23 @@ exports.postCourse = (req,res)=>{
         console.log(err);
         res.status(401).json({
             message:err
+        })
+    })
+}
+
+exports.deleteCourse = (req,res)=>{
+    Courses.findByIdAndDelete(req.params.id)
+    .exec()
+    .then(()=>{
+        res.status(200).json({
+            message:'Course deleted successfully'
+        });
+    })
+    .catch(err=>{
+        console.info('Code red');
+        console.error(err);
+        res.status(500).json({
+            message:'Internal server error'
         })
     })
 }
--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const courseControllers = require('../controllers/coursesController');
 
-router.route('/post/course').post(courseControllers.postCourse);
+router.route('/get/').get(courseControllers.getCourses);
+
+router.route('/get/:id').get(courseControllers.getById);
+
+router.route('/post').post(courseControllers.postCourse);
+
+router.route('/delete/:id').delete(courseControllers.deleteCourse);
 
 module.exports = router;
